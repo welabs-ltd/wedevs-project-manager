5201deb2bed269183fa114d23905bd1395735941
SamratEmily <nayakemily50@gmail.com>
2026-02-03 12:51:17 +0600

    removed commented lines

--- src/Comment/Transformers/Comment_Transformer.php
+++ src/Comment/Transformers/Comment_Transformer.php
@@ -28,20 +28,6 @@ class Comment_Transformer extends TransformerAbstract {
     ];
 
     public function transform( Comment $item ) {
-        // return [
-        //     'id'               => (int) $item->id,
-        //     'content'          => wedevs_pm_get_content( $item->content ),
-        //     'commentable_type' => $item->commentable_type,
-        //     'commentable_id'   => $item->commentable_id,
-        //     'created_at'       => wedevs_pm_format_date( $item->created_at ),
-        //     'updated_at'       => wedevs_pm_format_date( $item->updated_at ),
-        //     'project_id'       => (int) $item->project_id,
-        //     'meta'       => [
-        //         'total_replies' => $item->replies->count(),
-        //     ],
-
-        // ];
-
         $result = [
             'id'               => (int) $item->id,
             'content'          => wedevs_pm_get_content( $item->content ),
