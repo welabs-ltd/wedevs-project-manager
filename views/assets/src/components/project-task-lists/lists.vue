47a329ca507f026bea99411faceade4d1817dcd7
SamratEmily <nayakemily50@gmail.com>
2026-02-03 12:12:58 +0600

    removed 2 from modofication

--- views/assets/src/components/project-task-lists/lists.vue
+++ views/assets/src/components/project-task-lists/lists.vue
@@ -97,7 +97,7 @@
                                         </div>
 
                                         <div v-if="inboxList.expand" class="list-description">
-                                            <span>{{ __('This is a system default task list. Any task without an assigned tasklist will appear here 2.', 'wedevs-project-manager') }}</span>
+                                            <span>{{ __('This is a system default task list. Any task without an assigned tasklist will appear here.', 'wedevs-project-manager') }}</span>
                                         </div>
 
                                         <list-tasks 
